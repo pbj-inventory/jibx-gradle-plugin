68fb270e848cad50af8aa54c63c97c904bc45c47
Kaden Sharpin <kadensharpin@gmail.com>
2025-06-23 16:03:07 -0400

    Make the Jar task depend on the JibxTask

--- src/main/java/ca/cleaningdepot/tools/jibx/JibxPlugin.kt
+++ src/main/java/ca/cleaningdepot/tools/jibx/JibxPlugin.kt
@@ -3,19 +3,22 @@ package ca.cleaningdepot.tools.jibx
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.the
+import org.gradle.kotlin.dsl.withType
 
 class JibxPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val configuration = project.extensions.create<JibxPluginExtension>("jibx")
-        project.tasks.register<JibxTask>("jibx") {
+        val jibxTask = project.tasks.register<JibxTask>("jibx") {
             this.configuration = configuration
             this.classpath = project.configurations["compileClasspath"].plus(
                 project.the<JavaPluginExtension>().sourceSets.getByName("main").output
             )
         }
+        project.tasks.withType<Jar>().configureEach { dependsOn(jibxTask) }
     }
 }
