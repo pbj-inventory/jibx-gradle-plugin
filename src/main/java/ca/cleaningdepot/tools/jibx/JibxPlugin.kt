a9a2d3d0f0004f5c295dfd259761950e9a20610b
Kaden Sharpin <kadensharpin@gmail.com>
2026-05-07 22:57:39 -0400

    Fix gradle cache

--- src/main/java/ca/cleaningdepot/tools/jibx/JibxPlugin.kt
+++ src/main/java/ca/cleaningdepot/tools/jibx/JibxPlugin.kt
@@ -3,7 +3,6 @@ package ca.cleaningdepot.tools.jibx
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.DuplicatesStrategy
-import org.gradle.api.file.ProjectLayout
 import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.*
@@ -11,19 +10,16 @@ import org.gradle.kotlin.dsl.*
 class JibxPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         val configuration = project.extensions.create<JibxPluginExtension>("jibx")
+        val jibxDir = project.layout.buildDirectory.dir("jibx")
         val jibxTask = project.tasks.register<JibxTask>("jibx") {
             this.bindings.set(configuration.bindings)
             this.classpath.set(project.configurations["compileClasspath"])
             this.classRoots.set(project.the<JavaPluginExtension>().sourceSets.named("main").map { it.output })
+            this.output.set(jibxDir)
         }
         project.tasks.named<Jar>("jar") {
             duplicatesStrategy = DuplicatesStrategy.INCLUDE
-            from(project.layout.jibxDirectory)
-            dependsOn(jibxTask)
+            from(jibxTask)
         }
-    }
-
-    companion object {
-        val ProjectLayout.jibxDirectory get() = this.buildDirectory.dir("jibx")
     }
 }
--- src/main/java/ca/cleaningdepot/tools/jibx/JibxTask.kt
+++ src/main/java/ca/cleaningdepot/tools/jibx/JibxTask.kt
@@ -1,21 +1,15 @@
 package ca.cleaningdepot.tools.jibx
 
-import ca.cleaningdepot.tools.jibx.JibxPlugin.Companion.jibxDirectory
 import org.gradle.api.DefaultTask
+import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.file.FileCollection
-import org.gradle.api.file.ProjectLayout
-import org.gradle.api.file.RegularFile
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.*
 import org.jibx.binding.Compile
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
-import javax.inject.Inject
 
 @CacheableTask
-abstract class JibxTask @Inject constructor(private val layout: ProjectLayout) : DefaultTask() {
+abstract class JibxTask : DefaultTask() {
     @get:InputFiles
     @get:PathSensitive(PathSensitivity.RELATIVE)
     abstract val bindings: Property<FileCollection>
@@ -28,12 +22,12 @@ abstract class JibxTask @Inject constructor(private val layout: ProjectLayout) :
     @get:Classpath
     abstract val classRoots: Property<FileCollection>
 
-    @get:OutputFile
-    val output: Provider<RegularFile> = layout.jibxDirectory.map { it.file("jibx-output") }
+    @get:OutputDirectory
+    abstract val output: DirectoryProperty
 
     @TaskAction
     fun run() {
-        val jibxDir = layout.jibxDirectory.get().asFile
+        val jibxDir = output.get().asFile
         classRoots.get().filter { it.exists() }.forEach { it.copyRecursively(jibxDir, true) }
         val compiler = Compile()
         compiler.setLoad(false)
@@ -41,18 +35,6 @@ abstract class JibxTask @Inject constructor(private val layout: ProjectLayout) :
         compiler.setVerbose(false)
         compiler.setVerify(false)
         compiler.compile(classpath.get().files.plus(jibxDir).toStringArray(), bindings.get().files.toStringArray())
-        val hash = hash(
-            classpath.get().map { it.absolutePath.toByteArray() }
-                .plus(layout.jibxDirectory.get().asFileTree.map { it.readBytes() })
-                .plus(bindings.get().map { it.readBytes() })
-        )
-        output.get().asFile.writeText(hash)
-    }
-
-    private fun hash(items: Iterable<ByteArray>): String {
-        val sha256 = MessageDigest.getInstance("SHA-256")
-        items.forEach { sha256.update(it) }
-        return BigInteger(1, sha256.digest()).toString(16).padStart(sha256.digestLength * 2, '0')
     }
 
     private fun Set<File>.toStringArray() = this.map { it.absolutePath }.toTypedArray()
